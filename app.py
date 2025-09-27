293a2557490a39ec8ee0bf425c699baced790975
Aishwarya Patil <aish.p411@gmail.com>
2025-09-27 15:06:58 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from Flask!"
+
+@app.route('/health')
+def health():
+    return "OK", 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
